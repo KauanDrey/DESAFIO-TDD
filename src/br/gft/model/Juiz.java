93ee52f939d708ea0ec496c80476f76f505e3b78
Your Name <you@example.com>
2020-12-24 10:11:02 -0300

    Ciclo verde para 'papelGanhaDePedra' e criado todos os métodos

--- src/br/gft/model/Juiz.java
+++ src/br/gft/model/Juiz.java
@@ -1,7 +1,27 @@
 package br.gft.model;
 
 public class Juiz {
-	
-	
 
+	public String resposta(String playerOne, String playerTwo) {
+
+		if (playerOne.equals("Papel") && playerTwo.equals("Pedra")
+				|| playerOne.equals("Pedra") && playerTwo.equals("Papel")) {
+			return "Papel ganha de Pedra!";
+		}
+
+		else if (playerOne.equals("Pedra") && playerTwo.equals("Tesoura")
+				|| playerOne.equals("Tesoura") && playerTwo.equals("Pedra")) {
+			return "Pedra ganha de tesoura!";
+
+		} else if (playerOne.equals("Tesoura") && playerTwo.equals("Papel")
+				|| playerOne.equals("Papel") && playerTwo.equals("Tesoura")) {
+			return "Tesoura ganha de Papel!";
+
+		} else if (playerOne.equals(playerTwo)) {
+			return "Empate";
+
+		} else {
+			return "Erro na digitação da String";
+		}
+	}
 }
--- test/JuizTest.java
+++ test/JuizTest.java
@@ -17,5 +17,6 @@ public class JuizTest {
 		String mensagem = juiz.resposta("Papel", "Pedra");
 		System.out.println(mensagem);
 	}
+	
 
 }
